899a782ac0f9c87aaae9434d3760f1edadcb27f7
Farhan Hussain Khan <engnr_farhan_el@live.com>
2025-12-08 21:35:46 +0500

    Add files via upload

---
+++ helloSrv-container-ec2-docker/variables.tf
@@ -0,0 +1,10 @@
+variable "devopstest_VPC_CIDR" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "devopstest_SUBNET_CIDR" {
+  description = "CIDR block for the subnet"
+  default     = "10.0.1.0/24"
+}
+
